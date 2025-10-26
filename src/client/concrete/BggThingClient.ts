a1ecdde6f12dfd30e7dc15af9d073fc7088d9d63
Krzysztof Fijolek <fijolek.net@gmail.com>
2025-10-26 20:09:26 +0100

    BggThing: added alternate names

--- src/client/concrete/BggThingClient.ts
+++ src/client/concrete/BggThingClient.ts
@@ -1,11 +1,15 @@
-import { IDtoParser, BggThingDto } from "../../dto";
-import { IFetcher } from "../../fetcher";
-import { IRequestPaginator } from "../../paginator";
-import { IQueryBuilder } from "../../query";
-import { IThingRequest } from "../../request";
-import { IResponseParser } from "../../responseparser";
-import { PaginationRequestDto, ProgressResponseDto, QueryOptions } from "../dto";
-import { IBggThingClient } from "..";
+import { IDtoParser, BggThingDto } from '../../dto';
+import { IFetcher } from '../../fetcher';
+import { IRequestPaginator } from '../../paginator';
+import { IQueryBuilder } from '../../query';
+import { IThingRequest } from '../../request';
+import { IResponseParser } from '../../responseparser';
+import {
+  PaginationRequestDto,
+  ProgressResponseDto,
+  QueryOptions,
+} from '../dto';
+import { IBggThingClient } from '..';
 
 export class BggThingClient implements IBggThingClient {
   resource: string;
@@ -14,7 +18,7 @@ export class BggThingClient implements IBggThingClient {
   responseParser: IResponseParser<string, any>;
   dtoParser: IDtoParser<BggThingDto>;
   paginator: IRequestPaginator;
-  progressHandler: (progress: ProgressResponseDto<BggThingDto>) => void
+  progressHandler: (progress: ProgressResponseDto<BggThingDto>) => void;
 
   constructor(
     builder: IQueryBuilder<IThingRequest>,
@@ -23,7 +27,7 @@ export class BggThingClient implements IBggThingClient {
     dtoParser: IDtoParser<BggThingDto>,
     paginator: IRequestPaginator
   ) {
-    this.resource = "https://www.boardgamegeek.com/xmlapi2/thing";
+    this.resource = 'https://www.boardgamegeek.com/xmlapi2/thing';
     this.builder = builder;
     this.fetcher = fetcher;
     this.responseParser = responseParser;
@@ -32,12 +36,18 @@ export class BggThingClient implements IBggThingClient {
   }
 
   async query(request: IThingRequest): Promise<BggThingDto[]> {
-    if (Array.isArray(request.id) && (request.id as Array<number>).length > 50) {
-      const pagination: PaginationRequestDto<IThingRequest>[] = this.paginator.paginate<IThingRequest>(request, 50);
+    if (
+      Array.isArray(request.id) &&
+      (request.id as Array<number>).length > 50
+    ) {
+      const pagination: PaginationRequestDto<IThingRequest>[] =
+        this.paginator.paginate<IThingRequest>(request, 50);
 
-      let collection: BggThingDto[] = []
+      let collection: BggThingDto[] = [];
 
-      for await (const data of pagination.map(page => this.internalQuery(page.request))) {
+      for await (const data of pagination.map((page) =>
+        this.internalQuery(page.request)
+      )) {
         collection.push(...data);
       }
 
@@ -47,20 +57,25 @@ export class BggThingClient implements IBggThingClient {
     return this.internalQuery(request);
   }
 
-  async queryWithProgress(request: IThingRequest, progressOptions?: QueryOptions, progressHandler?: (progress: ProgressResponseDto<BggThingDto>) => void): Promise<void> {
-    const pagination: PaginationRequestDto<IThingRequest>[] = this.paginator.paginate<IThingRequest>(request, progressOptions?.limit);
+  async queryWithProgress(
+    request: IThingRequest,
+    progressOptions?: QueryOptions,
+    progressHandler?: (progress: ProgressResponseDto<BggThingDto>) => void
+  ): Promise<void> {
+    const pagination: PaginationRequestDto<IThingRequest>[] =
+      this.paginator.paginate<IThingRequest>(request, progressOptions?.limit);
     for (const page of pagination) {
       const data: Promise<BggThingDto[]> = this.internalQuery(page.request);
       progressHandler?.({
         current: page.current,
         total: page.total,
-        data: await data
+        data: await data,
       });
       this.progressHandler?.({
         current: page.current,
         total: page.total,
-        data: await data
-      })
+        data: await data,
+      });
     }
   }
 
--- src/dto/concrete/BggThingDto.ts
+++ src/dto/concrete/BggThingDto.ts
@@ -1,149 +1,173 @@
-import { JsonAlias, JsonClassType, JsonDeserialize, JsonIgnoreProperties, JsonManagedReference, JsonProperty } from "jackson-js";
-import { IBggDto } from "../interface";
-import { BggPollDto } from "./subdto/BggPollDto";
-import { BggStatisticsPaginatedDto, BggThingVideoPaginatedDto, BggThingCommentPaginatedDto} from "./paginated";
-import { BggLinkDto, BggThingMarketlistingsDto, BggThingVersionDto } from "./subdto";
+import {
+  JsonAlias,
+  JsonClassType,
+  JsonDeserialize,
+  JsonManagedReference,
+  JsonProperty,
+} from 'jackson-js';
+import { IBggDto } from '../interface';
+import { BggPollDto } from './subdto/BggPollDto';
+import {
+  BggStatisticsPaginatedDto,
+  BggThingVideoPaginatedDto,
+  BggThingCommentPaginatedDto,
+} from './paginated';
+import {
+  BggLinkDto,
+  BggThingMarketlistingsDto,
+  BggThingVersionDto,
+} from './subdto';
 
 export class BggThingDto implements IBggDto {
+  @JsonProperty()
+  @JsonClassType({ type: () => [Number] })
+  @JsonAlias({ values: ['@_id'] })
+  id!: number;
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [Number] })
-    @JsonAlias({ values: ["@_id"] })
-    id!: number;
+  @JsonProperty()
+  @JsonClassType({ type: () => [String] })
+  @JsonDeserialize({
+    using: (value: []) => value.map((item) => item['@_value'])[0],
+  })
+  name!: string;
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [String] })
-    @JsonDeserialize({
-        using: (value: []) => value.map(item => item['@_value'])[0]
-    })
-    name!: string;
+  @JsonProperty({ value: 'name' })
+  @JsonClassType({ type: () => [Array, [String]] })
+  @JsonAlias({ values: ['name'] })
+  @JsonDeserialize({
+    using: (value: any[]) => {
+      return Array.isArray(value)
+        ? value.map((item) => item?.['@_value']).filter(Boolean)
+        : [];
+    },
+  })
+  alternateNames!: string[];
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [String] })
-    @JsonAlias({ values: ["@_type"] })
-    type!: string;
+  @JsonProperty()
+  @JsonClassType({ type: () => [String] })
+  @JsonAlias({ values: ['@_type'] })
+  type!: string;
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [String] })
-    description!: string;
+  @JsonProperty()
+  @JsonClassType({ type: () => [String] })
+  description!: string;
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [String] })
-    thumbnail!: string;
+  @JsonProperty()
+  @JsonClassType({ type: () => [String] })
+  thumbnail!: string;
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [String] })
-    image!: string;
+  @JsonProperty()
+  @JsonClassType({ type: () => [String] })
+  image!: string;
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [Number] })
-    @JsonDeserialize({
-        using: (value: []) => value.map(item => item['@_value'])[0]
-    })
-    yearpublished!: number;
+  @JsonProperty()
+  @JsonClassType({ type: () => [Number] })
+  @JsonDeserialize({
+    using: (value: []) => value.map((item) => item['@_value'])[0],
+  })
+  yearpublished!: number;
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [Number] })
-    @JsonDeserialize({
-        using: (value: []) => value.map(item => item['@_value'])[0]
-    })
-    minplayers!: number;
+  @JsonProperty()
+  @JsonClassType({ type: () => [Number] })
+  @JsonDeserialize({
+    using: (value: []) => value.map((item) => item['@_value'])[0],
+  })
+  minplayers!: number;
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [Number] })
-    @JsonDeserialize({
-        using: (value: []) => value.map(item => item['@_value'])[0]
-    })
-    maxplayers!: number;
+  @JsonProperty()
+  @JsonClassType({ type: () => [Number] })
+  @JsonDeserialize({
+    using: (value: []) => value.map((item) => item['@_value'])[0],
+  })
+  maxplayers!: number;
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [Number] })
-    @JsonDeserialize({
-        using: (value: []) => value.map(item => item['@_value'])[0]
-    })
-    playingtime!: number;
+  @JsonProperty()
+  @JsonClassType({ type: () => [Number] })
+  @JsonDeserialize({
+    using: (value: []) => value.map((item) => item['@_value'])[0],
+  })
+  playingtime!: number;
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [Number] })
-    @JsonDeserialize({
-        using: (value: []) => value.map(item => item['@_value'])[0]
-    })
-    minplaytime!: number;
+  @JsonProperty()
+  @JsonClassType({ type: () => [Number] })
+  @JsonDeserialize({
+    using: (value: []) => value.map((item) => item['@_value'])[0],
+  })
+  minplaytime!: number;
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [Number] })
-    @JsonDeserialize({
-        using: (value: []) => value.map(item => item['@_value'])[0]
-    })
-    maxplaytime!: number;
+  @JsonProperty()
+  @JsonClassType({ type: () => [Number] })
+  @JsonDeserialize({
+    using: (value: []) => value.map((item) => item['@_value'])[0],
+  })
+  maxplaytime!: number;
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [Number] })
-    @JsonDeserialize({
-        using: (value: []) => value.map(item => item['@_value'])[0]
-    })
-    minage!: number;
+  @JsonProperty()
+  @JsonClassType({ type: () => [Number] })
+  @JsonDeserialize({
+    using: (value: []) => value.map((item) => item['@_value'])[0],
+  })
+  minage!: number;
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [Array, [BggLinkDto]] })
-    @JsonManagedReference()
-    @JsonAlias({ values: ["link"] })
-    links!: BggLinkDto[]
+  @JsonProperty()
+  @JsonClassType({ type: () => [Array, [BggLinkDto]] })
+  @JsonManagedReference()
+  @JsonAlias({ values: ['link'] })
+  links!: BggLinkDto[];
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [BggStatisticsPaginatedDto] })
-    @JsonManagedReference()
-    @JsonDeserialize({
-        //eslint-disable-next-line @typescript-eslint/no-explicit-any
-        using: (items: any[]) => items[0]
-    })
-    statistics!: BggStatisticsPaginatedDto;
+  @JsonProperty()
+  @JsonClassType({ type: () => [BggStatisticsPaginatedDto] })
+  @JsonManagedReference()
+  @JsonDeserialize({
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
+    using: (items: any[]) => items[0],
+  })
+  statistics!: BggStatisticsPaginatedDto;
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [BggThingVideoPaginatedDto] })
-    @JsonManagedReference()
-    @JsonDeserialize({
-        //eslint-disable-next-line @typescript-eslint/no-explicit-any
-        using: (items: any[]) => items[0]
-    })
-    videos!: BggThingVideoPaginatedDto;
+  @JsonProperty()
+  @JsonClassType({ type: () => [BggThingVideoPaginatedDto] })
+  @JsonManagedReference()
+  @JsonDeserialize({
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
+    using: (items: any[]) => items[0],
+  })
+  videos!: BggThingVideoPaginatedDto;
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [BggThingCommentPaginatedDto] })
-    @JsonManagedReference()
-    @JsonDeserialize({
-        //eslint-disable-next-line @typescript-eslint/no-explicit-any
-        using: (items: any[]) => items[0]
-    })
-    comments!: BggThingCommentPaginatedDto;
+  @JsonProperty()
+  @JsonClassType({ type: () => [BggThingCommentPaginatedDto] })
+  @JsonManagedReference()
+  @JsonDeserialize({
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
+    using: (items: any[]) => items[0],
+  })
+  comments!: BggThingCommentPaginatedDto;
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [Array, [BggThingMarketlistingsDto]] })
-    @JsonManagedReference()
-    @JsonDeserialize({
-        //eslint-disable-next-line @typescript-eslint/no-explicit-any
-        using: (items: any[]) => items[0]?.listing
-    })
-    marketplacelistings!: BggThingMarketlistingsDto[];
+  @JsonProperty()
+  @JsonClassType({ type: () => [Array, [BggThingMarketlistingsDto]] })
+  @JsonManagedReference()
+  @JsonDeserialize({
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
+    using: (items: any[]) => items[0]?.listing,
+  })
+  marketplacelistings!: BggThingMarketlistingsDto[];
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [Array, [BggPollDto]] })
-    @JsonManagedReference()
-    @JsonAlias({ values: ["poll"] })
-    polls!: BggPollDto[]
+  @JsonProperty()
+  @JsonClassType({ type: () => [Array, [BggPollDto]] })
+  @JsonManagedReference()
+  @JsonAlias({ values: ['poll'] })
+  polls!: BggPollDto[];
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [Array, [BggThingVersionDto]] })
-    @JsonManagedReference()
-    @JsonDeserialize({
-        //eslint-disable-next-line @typescript-eslint/no-explicit-any
-        using: (items: any[]) => items[0]?.item
-    })
-    versions!: BggThingVersionDto[];
+  @JsonProperty()
+  @JsonClassType({ type: () => [Array, [BggThingVersionDto]] })
+  @JsonManagedReference()
+  @JsonDeserialize({
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
+    using: (items: any[]) => items[0]?.item,
+  })
+  versions!: BggThingVersionDto[];
 
-    @JsonProperty()
-    @JsonClassType({ type: () => [Object] })
-    @JsonAlias({ values: ["poll-summary"] })
-    pollSummary!: any;
-
+  @JsonProperty()
+  @JsonClassType({ type: () => [Object] })
+  @JsonAlias({ values: ['poll-summary'] })
+  pollSummary!: any;
 }
